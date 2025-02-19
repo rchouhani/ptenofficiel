af4cf0fc599ed573bc827867cd1995c02aba1019
rchouhani <rchouhani@me.com>
2025-02-19 14:03:40 +0100

    ajout d'un tableau

---
+++ childDoctors.js
@@ -0,0 +1,18 @@
+const childDoctors = (
+    {
+        nom: 'Aubard',
+        prenom: 'Mélodie',
+        specialites: 'Neurologue',
+        adresse: 'Hopital Necker',
+        ville: 'Paris',
+        mail: 'melodie.aubard@aphp.fr'
+    },
+    {
+        nom:
+        prenom:
+        specialites:
+        adresse:
+        ville:
+        mail:
+    }
+)

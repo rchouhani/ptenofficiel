09f63d0a89df28ea54370b507b62b7644868dc04
rchouhani <rchouhani@me.com>
2025-02-20 21:11:18 +0100

    ajouts des docteurs présents sur le site ptenofficiel

--- childDoctors.js
+++ childDoctors.js
@@ -3,16 +3,135 @@ const childDoctors = (
         nom: 'Aubard',
         prenom: 'Mélodie',
         specialites: 'Neurologue',
-        adresse: 'Hopital Necker',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
         ville: 'Paris',
         mail: 'melodie.aubard@aphp.fr'
     },
     {
-        nom:
-        prenom:
-        specialites:
-        adresse:
-        ville:
-        mail:
+        nom: 'Guibaud',
+        prenom: 'Laurent',
+        specialites: 'Neurologie',
+        adresse: '59 Boulevard Pinel',
+        codePostal: '69677',
+        ville: 'Bron',
+        mail: 'laurent.guibaud@chu-lyon.fr'
+    },
+    {
+        nom: 'Sarret',
+        prenom: 'Catherine',
+        specialites: 'Génétique', 
+        adresse: 'CHU de Clermont Ferrand, Hopital d\'Estaing',
+        codePostal: '63003',
+        ville: 'Clermont-Ferrand',
+        mail: 'flaffargue@chu-clermontferrand.fr'
+    },
+    {
+        nom: 'Polak',
+        prenom: '',
+        specialites: 'Endocrino-Gynéco-Diabétologie',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
+        ville: 'Paris',
+        mail: 'patricia.miegakong@aphp.fr'
+    },
+    {
+        nom: 'Mignot',
+        prenom: 'Cyril',
+        specialites: 'Génétique',
+        adresse: 'Hopital de la Pitié-Salpêtrière, 47-83 Boulevard de l\'hopital',
+        codePostal: '75013',
+        ville: 'Paris',
+        mail: 'cyril.mignot@aphp.fr'
+    },
+    {
+        nom: 'Caux',
+        prenom: 'Frédéric',
+        specialites: 'Dermatologie',
+        adresse: 'Hopital Avicennes, 125 rue de Stalingrad',
+        codePostal: '93000',
+        ville: 'Bobigny',
+        mail: 'frederic.caux@aphp.fr'
+    },
+    {
+        nom: 'Passmard',
+        prenom: 'Sandrine',
+        specialites: 'Génétique',
+        adresse: 'Hopital Robert Debré, 48 Boulevard Servier',
+        codePostal: '75019',
+        ville: 'Paris',
+        mail: 'anne.gomati.mas@aphp.fr'
+    },
+    {
+        nom: 'Colas',
+        prenom: 'Chrystelle',
+        specialites: 'Génétique',
+        adresse: 'Institut Curie, 26 rue de l\'ULM',
+        codePostal: '75005',
+        ville: 'Paris',
+        mail: 'chrystelle.colas@curie.fr'
+    },
+    // {
+    //     nom: 'Viot',
+    //     prenom: 'Géraldine',
+    //     specialites: 'Génétique',
+    //     adresse: 'Clinique de la Fertilité, 26 rue Boissière',
+    //     codePostal: '75116',
+    //     ville: 'Paris',
+    //     mail: 'geraldine.viot@ahparis.org'
+    // }
+    {
+       nom: 'Cormier-Daire',
+       prenom: 'Valérie',
+       specialites: 'Génétique',
+       adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+       codePostal: '75015',
+       ville: 'Paris', 
+       mail: 'valerie.cormier-daire@aphp^.fr'
+    },
+    {
+        nom: 'Sarnacki',
+        prenom: 'Sabine', 
+        specialites: 'Chirurgie viscérale et urologique',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
+        ville: 'Paris',
+        mail: 'sabine.sarnacki@aphp.fr'
+    },
+    {
+        nom: 'Michot',
+        prenom: 'Caroline',
+        specialites: 'Génétique',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
+        ville: 'Paris',
+        mail: 'caroline.michot@aphp.fr'
+    },
+    {
+        nom: 'Hadj-Rabia',
+        prenom: 'Smaïl',
+        specialites: 'Dermatologie',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
+        ville: 'Paris',
+        mail: 'smail.hadj@inserm.fr'
+    },
+    {
+        nom: 'Benichi',
+        prenom: 'Sandra',
+        specialites: 'Neurochirurgie',
+        adresse: 'Hopital Necker-Enfants Malade, 149 rue de Sèvres',
+        codePostal: '75015',
+        ville: 'Paris',
+        mail: 'annie.dudot@aphp.fr'
+    },
+    {
+        nom: 'Coze',
+        prenom: 'Carole',
+        specialites: 'Oncologie',
+        adresse: 'Hopital de la Timone, 264 rue St Pierre',
+        codePostal: '13005',
+        ville: 'Marseille', 
+        mail: 'carole.coze@ap-hm.fr'
     }
 )

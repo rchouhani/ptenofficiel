1c4318dee14f370ea2aa71f527a6fc0e46bb6781
rchouhani <rchouhani@me.com>
2025-02-21 22:03:05 +0100

    Ajout d'une première liste

---
+++ adultsDoctors.js
@@ -0,0 +1,84 @@
+const adultsDoctors = (
+    {
+        nom: 'Delesalle',
+        prenom: 'Patrick',
+        specialites: 'Gastro-Entérologue',
+        adresse: 'Clinique du Palais, 25 avenue Chiris',
+        codePostal: '06130',
+        ville: 'Grasse',
+        mail: ''
+    },
+    {
+        nom: 'Giraud',
+        prenom: 'Sophie',
+        specialites: 'Onco-Généticienne',
+        adresse: '59 Boulevard Pinel',
+        codePostal: '69677',
+        ville: 'Bron',
+        mail: 'sophie.giraud@chu-lyon.fr'
+    },
+    {
+        nom: 'Casanova',
+        prenom: 'Sylvie',
+        specialites: 'Endocrinologue',
+        adresse: 'Centre cardiologique du Nord, 37 rue Moulin Gémeau',
+        codePostal: '93200',
+        ville: 'Saint-Denis',
+        mail: ''
+    },
+    {
+        nom: 'Russ',
+        prenom: 'Gilles',
+        specialites: 'Radiologie',
+        adresse: '14 Avenue René Coty',
+        codePostal: '75014',
+        ville: 'Paris',
+        mail: 'gilles.russ@wanadoo.fr'
+    },
+    {
+        nom: 'Fron',
+        prenom: 'Clémence',
+        specialites: 'Gastro-Entérologue',
+        adresse: 'Hopital de la Pitié-Salpêtrière, 47-83 Boulevard de l\'hopital',
+        codePostal: '75013',
+        ville: 'Paris',
+        mail: ''
+    },
+    {
+        nom: 'Caux',
+        prenom: 'Frédéric',
+        specialites: 'Dermatologie',
+        adresse: 'Hopital Avicennes, 125 rue de Stalingrad',
+        codePostal: '93000',
+        ville: 'Bobigny',
+        mail: 'frederic.caux@aphp.fr'
+    },
+    {
+        nom: 'Cusin',
+        prenom: 'Véronica',
+        specialites: 'Génétique',
+        adresse: 'Institut Curie, 26 rue de l\'ULM',
+        codePostal: '75005',
+        ville: 'Paris',
+        mail: ''
+    },
+    {
+        nom: 'Colas',
+        prenom: 'Chrystelle',
+        specialites: 'Génétique',
+        adresse: 'Institut Curie, 26 rue de l\'ULM',
+        codePostal: '75005',
+        ville: 'Paris',
+        mail: 'chrystelle.colas@curie.fr'
+    },
+    {
+       nom: 'Radu',
+       prenom: 'Alina',
+       specialites: 'Endocrinologue',
+       adresse: 'Hopital Européen Georges Pompidou, 20 rue Leblanc',
+       codePostal: '75015',
+       ville: 'Paris', 
+       mail: '',
+       lienAPHP: 'https://www.aphp.fr/offre-de-soin/medecin/3189317/075/63'
+    }
+)

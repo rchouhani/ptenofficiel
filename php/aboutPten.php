95c68696f836387027f112c81936526d7353e6b2
rchouhani <rchouhani@me.com>
2025-03-15 23:06:33 +0100

    le menu social est fixé

--- php/aboutPten.php
+++ php/aboutPten.php
@@ -8,6 +8,7 @@ $titre_page = "L'histoire de l'association";
 
 
 <div class="publications">
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7300432432602763264?collapsed=1" height="399" width="504" frameborder="0" allowfullscreen="" title="Post intégré"></iframe>
     <article>Je suis sur l'histoire de l'asso</article>
 </div>
 

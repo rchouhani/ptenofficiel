bb10b751390294affaa525ba1c0c5df4084b8ca3
rchouhani <rchouhani@me.com>
2025-04-08 17:20:11 +0200

    ajout de favicon

--- php/header.php
+++ php/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/favicon-16x16.png">
     <script defer type="module" src="../static/script.js"></script>
     <title><?php echo $titre_page; ?></title>
 </head>
